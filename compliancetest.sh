9b2753fbe1405fa06631bbd8e98bd280b916fe96
Michael Eager <michael.eager@monash.edu>
2014-05-13 12:15:52 +1000

    Debugging and testing scripts compliancetest and testfdf

---
+++ compliancetest.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+export PATH="./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/:$PATH"
+
+module load dcmtk
+
+dcmdump ./output_data_standard2d
+
+dcmdump  output_data/multiecho2d_magandphase/magnitude.dcm/0001.dcm
+
+dcmdump  output_data/multiecho2d_magandphase/phase.dcm/0001.dcm
+
+dcmdump  output_data/multiecho3d_magnitudeonly/0001.dcm
+
+dcmdump  output_data/cine/0001.dcm
+
+dcmdump  output_data/diffusion/0001.dcm
+
+dcmdump  output_data/ASL_se_06.dcm/0001.dcm
+
+
+
+# See http://www.dclunie.com/dicom3tools/dciodvfy.html for documentation
+
+ ./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/standard2d/0001.dcm 2>&1 >/dev/null | grep Required | uniq
+
+
+./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/multiecho3d_magonly/0001.dcm 2>&1 >/dev/null | grep Error | uniq
+
+./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/multiecho2d_magandphase/magnitude.dcm/0001.dcm 2>&1 >/dev/null | grep Error | uniq
+
+./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/multiecho2d_magandphase/phase.dcm/0001.dcm 2>&1 >/dev/null | grep Error | uniq 
+
+./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/cine/0001.dcm 2>&1 >/dev/null | grep Error | uniq
+
+./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/ASL_se_06.dcm/0001.dcm 2>&1 | grep Required | uniq
+
+ ./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/diffusion/0001.dcm 2>&1 >/dev/null | grep Required | uniq
---
+++ testfdf.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash 
+
+
+module load fslview/5.0.6 mrtrix/0.2.11
+
+mkdir output_nii
+
+rm -f output_nii/*.nii
+
+## standard 2d
+
+./Agilent2Dicom/fdf2dcm.sh -v -i ~/Monash016/amanda/ExampleAgilentData/standard2d/ -o ./output_data/standard2d
+mrconvert -info  output_data/standard2d/ output_nii/standard2d.nii
+fslview output_nii/standard2d.nii
+
+
+## Multi-echo 3D
+
+./Agilent2Dicom/fdf2dcm.sh -v -i  ~/Monash016/amanda/ExampleAgilentData/multiecho3d_magonly/ -o ./output_data/multiecho3d_magonly
+mrconvert -info  ./output_data/multiecho3d_magonly/ output_nii/ME3d.nii
+fslview output_nii/ME3d.nii
+
+
+## Multi-echo 2D Mag and phase
+
+./Agilent2Dicom/fdf2dcm.sh -v -i ~/Monash016/amanda/ExampleAgilentData/multiecho2d_magandphase/ -o  ./output_data/multiecho2d_magandphase
+mrconvert -info  output_data/multiecho2d_magandphase/magnitude.dcm output_nii/ME2d_mag.nii
+mrconvert -info  output_data/multiecho2d_magandphase/phase.dcm output_nii/ME2d_phase.nii
+fslview output_nii/ME2d_mag.nii output_nii/ME2d_phase.nii
+
+
+## CINE
+
+./Agilent2Dicom/fdf2dcm.sh -v -i  ~/Monash016/amanda/ExampleAgilentData/cine -o ./output_data/cine
+mrconvert -info  output_data/cine/ output_nii/CINE.nii
+fslview output_nii/CINE.nii
+
+
+## ASL
+
+./Agilent2Dicom/fdf2dcm.sh -v -i ./example_data/1008.2.40.4.1.1/ASL_se_06.img -o ./output_data/ASL_se_06.dcm
+mrconvert -info  output_data/ASL_se_06.dcm/ output_nii/ASL.nii
+fslview output_nii/ASL.nii
+
+#Diffusion
+
+./Agilent2Dicom/fdf2dcm.sh -v -i ~/Monash016/amanda/ExampleAgilentData/diffusion/ -o ./output_data/diffusion
+mrconvert -info  output_data/diffusion/ output_nii/Diffusion.nii
+fslview output_nii/Diffusion.nii
