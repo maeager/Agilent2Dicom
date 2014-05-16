f78020aaa21626a140c9383080ba478be6a88c23
Michael Eager <michael.eager@monash.edu>
2014-05-16 10:15:50 +1000

    Dicom modification after dcmulti conversion using dcmtools's dcmodify.

--- fdf2dcm.sh
+++ fdf2dcm.sh
@@ -24,7 +24,7 @@ export PATH=${PATH}:${DCM3TOOLS}
 E_BADARGS=65
 source ${FDF2DCMPATH}/yesno.sh
 
-DCMULTI="dcmulti -vvv  -makestack -sortby ImagePositionPatient  -dimension StackID FrameContentSequence -dimension InStackPositionNumber FrameContentSequence -of "
+DCMULTI="dcmulti -v  -makestack -sortby ImagePositionPatient  -dimension StackID FrameContentSequence -dimension InStackPositionNumber FrameContentSequence -of "
 
 
 # Print usage information and exit
@@ -172,6 +172,41 @@ if [ -f ${output_dir}/MULTIECHO ]; then
 else
     ${DCMULTI} ${output_dir}/0001.dcm $(ls -1 ${output_dir}/tmp/*.dcm)
 fi
+
+#if 0
+#then
+
+DCMODIFY=`which dcmodify`  #"$(dirname $0)/dmodify"
+files=$(find ${output_dir} -name "*.dcm")
+ # ${DCMODIFY} -m "(0020,0060)=" $files  # Laterality  # fixed in agilent2dicom
+ ${DCMODIFY} -i "(5200,9229)[0].(0018,9125)[0].(0018,1312)=ROW" $files # In-plane phase encoding direction
+
+firsttmpdcm=$(ls -1 ${output_dir}/tmp/*.dcm| head -1)
+declare -a FrameAnatomySequence=(`dcdump ${firsttmpdcm} 2>&1 >/dev/null | grep '(0x0008,0x010' | awk -F'>' '/</ {print $4}'| tr -d '<'`)
+echo "Frame Anatomy Seq: size: " ${#FrameAnatomySequence[*]}
+if [[ ${#FrameAnatomySequence[*]} -ne 8 ]];then
+    echo "DCM modification error. Not enough Frame Anatomy Sequence parameters."
+    echo " Ignoring Anatomy modifications."
+else
+echo "FrameAnt 7: " ${FrameAnatomySequence[7]}
+${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2218)[0].(0008,0100)=${FrameAnatomySequence[0]}" $files
+${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2218)[0].(0008,0104)=${FrameAnatomySequence[1]}" $files   #CodeMeaning=
+${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2218)[0].(0008,0102)=SRT" $files
+# ${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2220)[0].(0008,0100)=${FrameAnatomySequence[2]}" $files   #CodeValue=
+# ${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2220)[0].(0008,0104)=${FrameAnatomySequence[3]}" $files
+# ${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2220)[0].(0008,0102)=SRT" $files
+${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2228)[0].(0008,0100)=${FrameAnatomySequence[4]}" $files
+${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2228)[0].(0008,0104)=${FrameAnatomySequence[5]}" $files
+${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2228)[0].(0008,0102)=SRT" $files
+# ${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2230)[0].(0008,0100)=${FrameAnatomySequence[6]}" $files
+# ${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2230)[0].(0008,0104)=${FrameAnatomySequence[7]}" $files
+# ${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0008,2230)[0].(0008,0102)=SRT" $files
+${DCMODIFY} -i "(5200,9229)[0].(0020,9071)[0].(0020,9072)=R" $files
+#${DCMODIFY} -i "(0018,9125)[0].(0018,1312)=ROW" $files
+fi # array check
+echo "DCModify done"
+# fi #debugging
+
 
 
 ## Cleaning up
