e95556dffda3a9ec045473eabce8aa5ed89f69bb
Michael Eager <michael.eager@monash.edu>
2014-05-19 11:11:33 +1000

    fdf2dcm.sh: corrections and modifications moved to fix-dicoms.sh compliancetest.sh: removing dicom2tools path

--- compliancetest.sh
+++ compliancetest.sh
@@ -4,7 +4,7 @@ export PATH="./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/:$PAT
 
 module load dcmtk
 
-dcmdump ./output_data_standard2d
+dcmdump ./output_data/standard2d
 
 dcmdump  output_data/multiecho2d_magandphase/magnitude.dcm/0001.dcm
 
@@ -22,17 +22,17 @@ dcmdump  output_data/ASL_se_06.dcm/0001.dcm
 
 # See http://www.dclunie.com/dicom3tools/dciodvfy.html for documentation
 
- ./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/standard2d/0001.dcm 2>&1 >/dev/null | grep Required | uniq
+dciodvfy -dump ./output_data/standard2d/0001.dcm 2>&1 >/dev/null | grep Required | uniq
 
 
-./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/multiecho3d_magonly/0001.dcm 2>&1 >/dev/null | grep Error | uniq
+dciodvfy -dump ./output_data/multiecho3d_magonly/0001.dcm 2>&1 >/dev/null | grep Error | uniq
 
-./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/multiecho2d_magandphase/magnitude.dcm/0001.dcm 2>&1 >/dev/null | grep Error | uniq
+dciodvfy -dump ./output_data/multiecho2d_magandphase/magnitude.dcm/0001.dcm 2>&1 >/dev/null | grep Error | uniq
 
-./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/multiecho2d_magandphase/phase.dcm/0001.dcm 2>&1 >/dev/null | grep Error | uniq 
+dciodvfy -dump ./output_data/multiecho2d_magandphase/phase.dcm/0001.dcm 2>&1 >/dev/null | grep Error | uniq 
 
-./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/cine/0001.dcm 2>&1 >/dev/null | grep Error | uniq
+dciodvfy -dump ./output_data/cine/0001.dcm 2>&1 >/dev/null | grep Error | uniq
 
-./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/ASL_se_06.dcm/0001.dcm 2>&1 | grep Required | uniq
+dciodvfy -dump ./output_data/ASL_se_06.dcm/0001.dcm 2>&1 | grep Required | uniq
 
- ./dicom3tools_1.00.snapshot.20140306142442/bin/1.2.6.35.x8664/dciodvfy -dump ./output_data/diffusion/0001.dcm 2>&1 >/dev/null | grep Required | uniq
+ dciodvfy -dump ./output_data/diffusion/0001.dcm 2>&1 >/dev/null | grep Required | uniq
