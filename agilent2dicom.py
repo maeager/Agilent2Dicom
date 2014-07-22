40902285bf7242da9c66ad99aa924c01da49e220
Michael Eager <michael.eager@monash.edu>
2014-07-22 15:26:03 +1000

    agilent2dicom.py: Add MRImageFrameType sequence for Diffusion sequences.  Acquisition number using sorted Bvalue index

--- agilent2dicom.py
+++ agilent2dicom.py
@@ -1529,6 +1529,7 @@ if __name__ == "__main__":
 #                                                                frames in the SOP Instance. See
 #                                                                C.8.13.3.1.2 for a description and Defined
 #                                                                Terms.
+
 # C.8.13.3.1        MR Image Description Attribute Description
 # C.8.13.3.1.1      Complex Image Component
 # The value of the Complex Image Component attribute (0008,9208) shall be used to indicate
@@ -2159,7 +2160,7 @@ if __name__ == "__main__":
         if SEQUENCE == 'Diffusion':
             if args.verbose:
                 print 'Processing diffusion image'
-
+            ds.AcquisitionConstrast = ["DIFFUSION"]
             # Per frame Diffusion macro
                 
                 #print fdf_properties['array_index']
@@ -2179,7 +2180,7 @@ if __name__ == "__main__":
             if diffusion_idx > len(Bvalue):
                 print 'Procpar Bvalue does not contain enough values determined by fdf_properties array_index'
 
-            ds.AcquisitionNumber = BValueSortIdx[diffusion_idx] #fdf_properties['array_index']
+            ds.AcquisitionNumber = BValueSortIdx[diffusion_idx] # Sort diffusion based on sorted index of Bvalue instead of fdf_properties['array_index']
             
             if (math.fabs(Bvalue[ diffusion_idx ] - fdf_properties['bvalue']) > 0.005):
                 print 'Procpar and fdf B-value mismatch: procpar value ', Bvalue[ diffusion_idx ], ' and  local fdf value ', fdf_properties['bvalue'], ' array idx ', fdf_properties['array_index'] 
@@ -2209,6 +2210,43 @@ if __name__ == "__main__":
             diffusionseq.DiffusionGradientDirectionSequence = Sequence([diffbmatseq])
 
             ds.MRDiffusionSequence= Sequence([diffusionseq])
+
+            MRImageFrameType = Dataset()
+            MRImageFrameType.FrameType=ds.ImageType
+            MRImageFrameType.PixelPresentation=["MONOCHROME"]
+            MRImageFrameType.VolumetrixProperties=["VOLUME"]
+            MRImageFrameType.VolumeBasedCalculationTechnique=["NONE"]
+            MRImageFrameType.ComplexImageComponent=["MAGNITUDE"]
+            MRImageFrameType.AqcuisitionContrast=["DIFFUSION"]
+            ds.MRImageFrameTypeSequence=Sequence([MRImageFrameType])
+            
+    # Standard 3T diffusion scan 
+    # (0018,9226) SQ (Sequence with undefined length #=1)     # u/l, 1 MRImageFrameTypeSequence
+    #   (fffe,e000) na (Item with undefined length #=12)        # u/l, 1 Item
+    #     (0008,9007) CS [ORIGINAL\PRIMARY\DIFFUSION\NONE]        #  32, 4 FrameType
+    #     (0008,9205) CS [MONOCHROME]                             #  10, 1 PixelPresentation
+    #     (0008,9206) CS [VOLUME]                                 #   6, 1 VolumetricProperties
+    #     (0008,9207) CS [NONE]                                   #   4, 1 VolumeBasedCalculationTechnique
+    #     (0008,9208) CS [MAGNITUDE]                              #  10, 1 ComplexImageComponent
+    #     (0008,9209) CS [DIFFUSION]                              #  10, 1 AcquisitionContrast
+    #     (0021,0010) LO [SIEMENS MR SDI 02]                      #  18, 1 PrivateCreator
+    #     (0021,1059) IS [0\0\0]                                  #   6, 3 Unknown Tag & Data
+    #     (0021,1076) LO [ChannelMixing:ND=true_CMM=1_CDM=1\ACC1\NormalizeAlgo:PreScan] #  60, 3 Unknown Tag & Data
+    #     (0021,1077) LO [*ep_b2000#1]                            #  12, 1 Unknown Tag & Data
+    #     (0021,1078) CS [NONE\ND\NORM]                           #  12, 3 Unknown Tag & Data
+    #     (0021,1079) CS [ND]                                     #   2, 1 Unknown Tag & Data
+    #   (fffe,e00d) na (ItemDelimitationItem)                   #   0, 0 ItemDelimitationItem
+    # (fffe,e0dd) na (SequenceDelimitationItem)               #   0, 0 SequenceDelimitationItem
+    # (0020,9111) SQ (Sequence with undefined length #=1)     # u/l, 1 FrameContentSequence
+    #   (fffe,e000) na (Item with undefined length #=7)         # u/l, 1 Item
+    #     (0018,9074) DT [20140505154610.630000]                  #  22, 1 FrameAcquisitionDateTime
+    #     (0018,9151) DT [20140505154610.647500]                  #  22, 1 FrameReferenceDateTime
+    #     (0018,9220) FD 35                                       #   8, 1 FrameAcquisitionDuration
+    #     (0020,9056) SH [1]                                      #   2, 1 StackID
+    #     (0020,9057) UL 1                                        #   4, 1 InStackPositionNumber
+    #     (0020,9128) UL 2                                        #   4, 1 TemporalPositionIndex
+    #     (0020,9156) US 2                                        #   2, 1 FrameAcquisitionNumber
+    #   (fffe,e00d) na (ItemDelimitationItem)                   #   0, 0 ItemDelimitationItem
 
 ##
         
@@ -2389,7 +2427,6 @@ if __name__ == "__main__":
         ds.RescaleIntercept = str(RescaleIntercept)                                #(0028,1052) Rescale Intercept
         # Rescale slope string must not be longer than 16
         ds.RescaleSlope = str(RescaleSlope)[:16]                                       #(0028,1053) Rescale Slope
-
 
         #ds.MRAcquistionType = '2D'                                              # 0018,0023 MR Acquisition Type (optional)
                                                                                          # Identification of spatial data encoding scheme.
