4ed2d6321c7c32f436bd247b14d5824e91dd66c0
Michael Eager <michael.eager@monash.edu>
2014-09-05 14:04:04 +1000

    fdf2dcm.sh and fix_asl.sh Implement ASL exporting capability of agilent2dicom

--- fix_asl.sh
+++ fix_asl.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
 
+
+
+# dcdump ../output_data/ASL_se_06.dcm/tmp/slice001image001echo001.dcm 2>&1| grep -A30 'MR Arterial' | sed 's/.*VL=<[^>]*>\s\(.*\)/\1/'                                
 
 
 # (0x0018,0x9251) SQ MR Arterial Spin Labeling Sequence    VR=<SQ>   VL=<0xffffffff>
@@ -25,80 +29,57 @@
 #     > (0x0008,0x0100) SH Code Value      VR=<SH>   VL=<0x0006>  <G-A138>
 #     > (0x0008,0x0104) LO Code Meaning    VR=<LO>   VL=<0x0008>  <Coronal >
 
-
 #     > (0x0018,0x9253) US ASL Slab Number         VR=<US>   VL=<0x0002>  [0x0001]
 #     > (0x0018,0x9254) FD ASL Slab Thickness      VR=<FD>   VL=<0x0008>  {12}
 #     > (0x0018,0x9255) FD ASL Slab Orientation    VR=<FD>   VL=<0x0018>  {1.38,180.661,87.387}
 #     > (0x0018,0x9256) FD ASL Mid Slab Position   VR=<FD>   VL=<0x0018>  {0,0,0}
 #     > (0x0018,0x9258) UL ASL Pulse Train Duration        VR=<UL>   VL=<0x0004>  [0x00000000]
-
-
-
-
-(0018,9251)[0]. 
-  ----:               
-    (0018,9251)[0].(0x0018,0x9250) 
-    (0018,9251)[0].(0x0018,0x9252) 
-    (0018,9251)[0].(0x0018,0x9257) 
-    (0018,9251)[0].(0x0018,0x9259) 
-    (0018,9251)[0].(0x0018,0x925b) 
-    (0018,9251)[0].(0x0018,0x925c) 
-    (0018,9251)[0].(0x0018,0x925d) 
-  ----:               
-    (0018,9251)[0].(0018,925d)[0].(0x0018,0x925e) 
-    (0018,9251)[0].(0018,925d)[0].(0x0018,0x925f) 
                       
-    (0018,9251)[0].(0x0018,0x9260) 
-  ----:               
-    (0018,9251)[0].(0018,9260)[0].(0x0018,0x9253) 
-    (0018,9251)[0].(0018,9260)[0].(0x0018,0x9254) 
-    (0018,9251)[0].(0018,9260)[0].(0x0018,0x9255) 
-    (0018,9251)[0].(0018,9260)[0].(0x0018,0x9256) 
-    (0018,9251)[0].(0018,9260)[0].(0x0018,0x9258) 
-
-    (0018,9251)[0].(0018,9260)[0].(0x0008,0x2218)[0].(0x0008,0x0100) 
-    (0018,9251)[0].(0018,9260)[0].(0x0008,0x2218)[0].(0x0008,0x0104) 
-
-    (0018,9251)[0].(0018,9260)[0].(0x0008,0x2220)[0].(0x0008,0x0100) 
-    (0018,9251)[0].(0018,9260)[0].(0x0008,0x2220)[0].(0x0008,0x0104) 
-                      
-                      
-
-dcdump ../output_data/ASL_se_06.dcm/tmp/slice001image001echo001.dcm 2>&1| grep -A30 'MR Arterial' | sed 's/.*VL=<[^>]*>\s\(.*\)/\1/'                                
-# per frame (0x5200,0x9230)
 
 
     firsttmpdcm=$(ls -1 "${output_dir}"/tmp/*.dcm| head -1)
+    dcdump "${firsttmpdcm}" 2>&1| grep -A30 'MR Arterial' | sed -e 's/.*VL=<[^>]*>\s*\(.*\)/\1/' -e '/---/d' -e '/^$/d' | tr -d '<>{}[]' > "${output_dir}"/arterial.tmp
 
-
-    dcdump "${firsttmpdcm}" 2>&1 >/dev/null | grep '(0x0008,0x010' | awk -F'>' '/</ {print $4}'| tr -d '<' > "${output_dir}"/anatomy.tmp
-
-
-    if [ -f  ${output_dir}/anatomy.tmp ]; then
-	declare -a FrameAnatomySequence
+    if [ -f  ${output_dir}/arterial.tmp ]; then
+	declare -a ArterialSpinLabelling
 	let i=0;while IFS=$'\r\n' read -r line_data; do 
-	    FrameAnatomySequence[i]="${line_data}"; ((++i)); 
-	done < "${output_dir}"/anatomy.tmp
-	rm -f "${output_dir}"/anatomy.tmp
+	    ArterialSpinLabelling[i]="${line_data}"; ((++i)); 
+	done < "${output_dir}"/arterial.tmp
+	rm -f "${output_dir}"/arterial.tmp
     else
-	echo "Cannot find anatomy.tmp in output dir"
+	echo "Cannot find arterial.tmp in output dir"
     fi
 
-    echo "Frame Anatomy Seq: ${firsttmpdcm} size: " ${#FrameAnatomySequence[*]}
-    if [[ ${#FrameAnatomySequence[*]} -ne 8 ]];then
-	echo "DCM modification error. Not enough Frame Anatomy Sequence parameters."
-	echo " Ignoring Anatomy modifications."
+    echo "ASL : ${firsttmpdcm} size: " ${#ArterialSpinLabelling[*]}
+    if [[ ${#ArterialSpinLabelling[*]} -ne 17 ]];then
+	echo "DCM modification error. Not enough ASL parameters."
+	echo " Ignoring ASL modifications."
     else
-	echo "FrameAnt 7: " ${FrameAnatomySequence[7]}
-	${DCMODIFY} -i "(0018,9251)[0].(0020,9071)[0].(0008,2218)[0].(0008,0100)=${FrameAnatomySequence[0]}" $files
-	${DCMODIFY} -i "(0018,9251)[0].(0020,9071)[0].(0008,2218)[0].(0008,0104)=${FrameAnatomySequence[1]}" $files   #CodeMeaning=
-	${DCMODIFY} -i "(0018,9251)[0].(0020,9071)[0].(0008,2218)[0].(0008,0102)=SRT" $files
+	echo "FrameAnt 7: " ${ArterialSpinLabelling[7]}
 
-	${DCMODIFY} -i "(0018,9251)[0].(0020,9071)[0].(0008,2228)[0].(0008,0100)=${FrameAnatomySequence[4]}" $files
-	${DCMODIFY} -i "(0018,9251)[0].(0020,9071)[0].(0008,2228)[0].(0008,0104)=${FrameAnatomySequence[5]}" $files
-	${DCMODIFY} -i "(0018,9251)[0].(0020,9071)[0].(0008,2228)[0].(0008,0102)=SRT" $files
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9250)=${ArterialSpinLabelling[0]}" $files	   #  Arterial Spin Labeling Contrast	      VR=<CS>	VL=<000a>
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9252)=${ArterialSpinLabelling[1]}" $files     # ASL Technique Description       VR=<LO>   VL=<0004>  <FAIR> 
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9257)=${ArterialSpinLabelling[2]}" $files	   #ASL Context	    VR=<CS>   VL=<0006>  <LABEL > 
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9259)=${ArterialSpinLabelling[3]}" $files     #ASL Crusher Flag        VR=<CS>   VL=<0002>  <NO>   
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,925b)=${ArterialSpinLabelling[4]}" $files	   #ASL Crusher Description	    VR=<LO>   VL=<0014>  <crusher description > 
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,925c)=${ArterialSpinLabelling[5]}" $files     #ASL Bolus Cut-off Flag          VR=<CS>   VL=<0002>  <NO>
+    # ASL Bolus Cut-off Timing Sequence
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,925d)[0].(0018,925e)=${ArterialSpinLabelling[6]}" $files	#ASL Bolus Cut-off Technique	 VR=<LO>   VL=<0000>	<>	     
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,925d)[0].(0018,925f)=${ArterialSpinLabelling[7]}" $files	#ASL Bolus Cut-off Delay Time	 VR=<UL>   VL=<0004>	[00000000]
+    # ASL Slab Sequence	
+    #    Anatomic Region Sequence
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0008,2218)[0].(0008,0100)=${ArterialSpinLabelling[8]}" $files  #Code Value	   VR=<SH>   VL=<0008>  <T-D1100 > 
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0008,2218)[0].(0008,0104)=${ArterialSpinLabelling[9]}" $files  #Code Meaning    VR=<LO>   VL=<0004>  <Head>     
+    #    Anatomic Region Modifier Sequence 
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0008,2220)[0].(0008,0100)=${ArterialSpinLabelling[10]}" $files #Code Value	   VR=<SH>   VL=<0006>  <G-A138>   
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0008,2220)[0].(0008,0104)=${ArterialSpinLabelling[11]}" $files #Code Meaning    VR=<LO>   VL=<0008>  <Coronal > 
 
-	${DCMODIFY} -i "(0018,9251)[0].(0020,9071)[0].(0020,9072)=R" $files
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0018,9253)=${ArterialSpinLabelling[12]}" $files #ASL Slab Number		VR=<US>	  VL=<0002>  [0001]
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0018,9254)=${ArterialSpinLabelling[13]}" $files #ASL Slab Thickness      VR=<FD>   VL=<0008>  {12}
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0018,9255)=${ArterialSpinLabelling[14]}" $files #ASL Slab Orientation	VR=<FD>	  VL=<0018>  {1.38,180.661,87.387}
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0018,9256)=${ArterialSpinLabelling[15]}" $files #ASL Mid Slab Position   VR=<FD>   VL=<0018>  {0,0,0}
+    ${DCMODIFY} -i "(0018,9251)[0].(0018,9260)[0].(0018,9258)=${ArterialSpinLabelling[16]}" $files #ASL Pulse Train Duration	VR=<UL>	  VL=<0004>  [00000000]
+ 
 
     fi # array check
     echo "DCModify ASL done"
