d4fd9f0633c6519599e1f9087275c1719be64c34
Michael Eager <michael.eager@monash.edu>
2014-10-21 15:56:32 +1100

    Add copyright to Agilent2DicomApp

--- Agilent2DicomApp.py
+++ Agilent2DicomApp.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+#
+#  Agilent2DicomApp the PyQy4 GUI for the Agilent console
+#
+# - Monash Biomedical Imaging
+# - Michael Eager (michael.eager@monash.edu)
+
+#   Copyright (C) 2014 Michael Eager  (michael.eager@monash.edu)
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
