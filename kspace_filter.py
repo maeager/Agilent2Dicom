b2dbe0d1ff583facca03a0f3bf00ceffd41bb963
Michael Eager <michael.eager@monash.edu>
2014-12-12 15:10:58 +1100

    kspace_filter: epanechnikov imported in fourierepanechnikov

--- kspace_filter.py
+++ kspace_filter.py
@@ -174,6 +174,7 @@ def fourierepanechnikov(siz,sigma):
     uu = xx[np.newaxis,:,np.newaxis]*mult_fact + np.spacing(1)
     vv = yy[:,np.newaxis,np.newaxis]*mult_fact + np.spacing(1)
     ww = zz[np.newaxis,np.newaxis,:]*mult_fact + np.spacing(1)
+    from cplxfilter import epanechnikov
     Kepa=epanechnikov([np.ceil(sigma)+2,np.ceil(sigma)+2,np.ceil(sigma)+2],sigma)
     K=np.zeros(np.array(siz), dtype=numpy.float32)  
     szmin = np.floor(sz/2.0 - np.array(Kepa.shape)/2.0)
