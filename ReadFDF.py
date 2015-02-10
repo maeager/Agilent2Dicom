38e5e7fdba8d73ad91da4d6867badd4c28af1d5b
Michael Eager <michael.eager@monash.edu>
2015-02-10 15:01:29 +1100

    ReadFDF.py: builtin compile preferred over exec

--- ReadFDF.py
+++ ReadFDF.py
@@ -59,7 +59,7 @@ def ReadFDF(fdffilename):
         tokens[1] = tokens[1].strip()
         if tokens[1][0] == '{' and tokens[1][-1] == '}':
             tokens[1] = '[' + tokens[-1].strip('{}') + ']'
-        exec('fdf_properties["' + tokens[0] + '"] = ' + tokens[1])
+        compile('fdf_properties["' + tokens[0] + '"] = ' + tokens[1], "<string>", "exec")
         line = f.readline()
     fdf_properties['filename'] = fdffilename
     fdf_properties['filetext'] = fdftext
