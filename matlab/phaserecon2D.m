0ad14812c05e7b9df6feec2fd48b6c4985da9a10
Michael Eager <mick.eager@gmail.com>
2015-09-21 09:19:43 +1000

    Phase recon for 2D slices handled by phaserecon2D. v1 updated.

--- matlab/phaserecon2D.m
+++ matlab/phaserecon2D.m
@@ -1,6 +1,6 @@
-function [pha, swi_n, swi_p, mag] = phaserecon_v1(kimg,kimgsos,a,intpl,thr)
+function [pha, swi_n, swi_p, mag] = phaserecon2D(kimg,kimgsos,a,intpl,thr)
 % Usage: calculate phase images from complex inputs
-% Inputs: 
+% Inputs:
 %        kimg -- reconstructed kspace signals
 %        kimgsos -- sos of square on complex images
 %        a    -- gaussian filter size 0 to 20, 10 is good balance
@@ -13,27 +13,27 @@ function [pha, swi_n, swi_p, mag] = phaserecon_v1(kimg,kimgsos,a,intpl,thr)
 %        swi -- phase weighted magnatitue images
 %
 % Zhaolin Chen @ Howard Florey Institute
-% log            12-05-09 SWI part fixed 
+% log            12-05-09 SWI part fixed
+%
+% Amendents:
+% Michael Eager Monash Biomedical Imaging
+% 09-2015 2D implementation of homodyne filter
 % -------------------------------------------
 
 
 [Nfe,Npe,Npe2] = size(kimg);
 
-multi_dim=0;
-if Npe2 ~= size(kimg,3)
-    display ('Multi dim kimg')
-    Npe2 = size(kimg,3);
-    multi_dim=1;
-end
+
 
 % create a Gaussian LPF
-win2 = gausswin(Nfe,a)*gausswin(Npe,a)';win3 = 1;%win3=gausswin(Npe2,a);
-win=zeros(Nfe,Npe,Npe2);
-for k=1:Npe2
-    win(:,:,k)=win2;%*win3(k);
-end
+win = gausswin(Nfe,a)*gausswin(Npe,a)';
+%win3=gausswin(Npe2,a);
+%win=zeros(Nfe,Npe,Npe2);
+%for k=1:Npe2
+%    win(:,:,k)=win2*win3(k);
+%end
 win=win./(max(win(:)));
-    
+
 %win=fspecial3('gaussian',[Nfe,Npe,Npe2],a);
 % creat a rectangular LPF
 % win = zeros(Nfe,Npe);
@@ -44,44 +44,51 @@ win=win./(max(win(:)));
 % creat a Kaiser LPF
 %win = hann(Nfe)*hann(Npe)';
 %win = kaiser(Nfe,a)*kaiser(Npe,a)';
-
-[xx,yy,zz] = find(kimg == max(kimg(:)));
-win = circshift(win,[xx,yy,zz]-[floor(Nfe/2), floor(Npe/2), floor(Npe2/2)]);
-
-
+if numel(size(kimg)==5)
+    kimg2=kimg(:,:,:,1,1);
+    [xx,yy,zz] = find(kimg2 == max(kimg2(:)));
+else
+   [xx,yy,zz] = find(kimg == max(kimg(:)));
+end
+win = circshift(win,[xx,yy]-[floor(Nfe/2), floor(Npe/2)]);
 
 img = fftshift(fftn(kimg));%,[intpl*Nfe,intpl*Npe,intpl*Npe2]));
 imgsos = fftshift(fftn(kimgsos));%,[intpl*Nfe,intpl*Npe,intpl*Npe2]));
 
-if multi_dim ==1
-    for i=1:size(kimg,5)
-    img_lpf(:,:,:,1,i) = fftshift(fftn(kimg(:,:,:,1,i).*win));
+if numel(size(kimg)==5)
+    for echo=1:size(kimg,5)
+        for channel=1:size(kimg,4)
+            for slice=1:size(kimg,3)
+                img_lpf(:,:,slice,channel,echo) = fftshift(fft2(kimg(:,:,slice,channel,echo).*win));
+            end
+        end
     end
 else
-
-img_lpf = fftshift(fftn(kimg.*win));%,[intpl*Nfe,intpl*Npe,intpl*Npe2]));
+    for slice=1:size(kimg,3)
+        img_lpf(:,:,slice) = fftshift(fft2(kimg(:,:,slice).*win));
+    end
 end
 
 img_hpf = img ./ img_lpf;
 
 mag = abs(img);
 
-thd = (thr/sqrt(sqrt(intpl)))*max(abs(imgsos(:))); 
+thd = (thr/sqrt(sqrt(intpl)))*max(abs(imgsos(:)));
 
 
 pha = angle(img_hpf);
 
 
 if (thr ~= 0)
-%    for i = 1:intpl*Nfe
-%        for j = 1:intpl*Npe
-%            if (abs(imgsos(i,j)) <= thd)
-%               pha(i,j) = pha(i,j)/10;
-%            end
-%        end
-%    end
-   x = find(abs(imgsos(:)) <= thd);
-   pha(x)=pha(x)/10;
+    %    for i = 1:intpl*Nfe
+    %        for j = 1:intpl*Npe
+    %            if (abs(imgsos(i,j)) <= thd)
+    %               pha(i,j) = pha(i,j)/10;
+    %            end
+    %        end
+    %    end
+    x = find(abs(imgsos(:)) <= thd);
+    pha(x)=pha(x)/10;
 end
 
 %mag = sqrt(mag);
