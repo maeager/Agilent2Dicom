57b28e4907b19bfef44b47d25e5bbf601f757aef
Michael Eager <mick.eager@gmail.com>
2015-11-09 10:37:21 +1100

    minIP: allow MIP in different axes with 'dim' arg

--- matlab/NLmeans/minIP.m
+++ matlab/NLmeans/minIP.m
@@ -1,11 +1,25 @@
-function [mIP]=minIP(mag,slab)
+function [mIP] = minIP(mag, slab, dim)
 %%minimum intensity projection
-% 2D minimum of 3D slab  
+% minimum projection of 3D sections slab  
 % 
 % -(C) 2015  Michael Eager (michael.eager@monash.edu)
 
-
-for i =1:size(mag,3)
-    start = i;endp = min(size(mag,3), i+slab);
-    mIP(:,:,i) = min(mag(:,:,start:endp),[],3);
+if nargin < 3 || dim == 3
+    for i =1:size(mag,3)
+        start = i;endp = min(size(mag,3), i+slab);
+        mIP(:,:,i) = min(mag(:,:,start:endp),[],3);
+    end
+else
+    if dim == 1
+        
+        for i =1:size(mag,1)
+            start = i;endp = min(size(mag,1), i+slab);
+            mIP(:,:,i) = squeeze(min(mag(start:endp,:,:),[],1));
+        end
+    elseif dim == 2
+        for i =1:size(mag,2)
+            start = i;endp = min(size(mag,2), i+slab);
+            mIP(:,:,i) = squeeze(min(mag(:,start:endp,:),[],2));
+        end
+    end
 end
