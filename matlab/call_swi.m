1cfe0b4281d5b7c1c6eee7fdf5d359a298f237bc
Michael Eager <mick.eager@gmail.com>
2016-02-10 14:49:52 +1100

    call_swi:  fix quotes in input strings, add support for 5D images

--- matlab/call_swi.m
+++ matlab/call_swi.m
@@ -11,9 +11,9 @@ addpath(fullfile(root_path,'matlab'))
 addpath(fullfile(root_path,'matlab/NIFTI'))
 addpath(fullfile(root_path, 'matlab/Agilent/'))
 %% Clean input strings
-in1 = regexprep(in1,'["\[\]]','');  
+in1 = regexprep(in1,'["\[\]]','');
 if ~isempty(in2)
- if isstr(in2)
+    if isstr(in2)
         in2 = regexprep(in2,'["\[\]]','');
     else
         in2=[];
@@ -37,17 +37,17 @@ if nargin < 6
     saveRI=0;
 end
 if nargin < 5
-    preprocess=0;    
+    preprocess=0;
 end
 if nargin < 4
-    order=0;    
+    order=0;
 end
-    
+
 
 voxelsize=[];
 ksp1=[];ksp2=[];
 
-if exist(in1,'file')==2 && ~isempty(strfind(in1,'.nii')) 
+if exist(in1,'file')==2 && ~isempty(strfind(in1,'.nii'))
     nii1_in=load_nii(in1);
     img=nii1_in.img;
     ksp1=fftn(img);
@@ -68,52 +68,75 @@ else
 end
 
 if ~isempty(in2)
-if exist(in2,'file')==2 && ~isempty(strfind(in2,'.nii')) 
-    nii2_in=load_nii(in2);
-    img=nii2_in.img;
-    ksp2=fftn(img);
-    voxelsize2=nii2_in.dime.pixdim(2:4);
-elseif ~isempty(strfind(in2,'.img')) && isdir(in2)
-    [img hdr] =readfdf(in2);
-    %    voxelsize=hdr.FOVcm/size(img)*10;
-    ksp2=fftn(img);
-    %voxelsize2=hdr.roi*10/hdr.matrix;
-    voxelsize2 = hdr.voxelsize*10;
-elseif ~isempty(strfind(in2,'.fid')) && isdir(in2)
-    [img, hdr, ksp2, RE, IM] = readfid(in2);
-    %    voxelsize2=hdr.FOVcm*10/size(img);
-    voxelsize2=hdr.voxelmm;
-else
-    display(['Cannot find second image ' in2])
-    %    return
-end
+    if exist(in2,'file')==2 && ~isempty(strfind(in2,'.nii'))
+        nii2_in=load_nii(in2);
+        img=nii2_in.img;
+        ksp2=fftn(img);
+        voxelsize2=nii2_in.dime.pixdim(2:4);
+    elseif ~isempty(strfind(in2,'.img')) && isdir(in2)
+        [img hdr] =readfdf(in2);
+        %    voxelsize=hdr.FOVcm/size(img)*10;
+        ksp2=fftn(img);
+        %voxelsize2=hdr.roi*10/hdr.matrix;
+        voxelsize2 = hdr.voxelsize*10;
+    elseif ~isempty(strfind(in2,'.fid')) && isdir(in2)
+        [img, hdr, ksp2, RE, IM] = readfid(in2);
+        %    voxelsize2=hdr.FOVcm*10/size(img);
+        voxelsize2=hdr.voxelmm;
+    else
+        display(['Cannot find second image ' in2])
+        %    return
+    end
 end
 
 
 if ~isempty(in2) && sum(voxelsize1) ~= sum(voxelsize2)
-  display(['Voxelsizes don''t match: ' str2num(voxelsize1) ' ' ...
-           str2num(voxelsize2)])
-  return
+    display(['Voxelsizes don''t match: ' str2num(voxelsize1) ' ' ...
+        str2num(voxelsize2)])
+    return
 end
 voxelsize=voxelsize1;
 
-
-[pha, swi_n, swi_p, mag] = phaserecon_v1(ksp1,ksp1,0.4,1,0.05);
-% Necessary translations to match FDF images
-swi_n=flipdim(flipdim(flipdim(swi_n,1),2),3);
-swi_n=circshift(swi_n,[1,1,1]);
-img=mag.*exp(1i*pha);
-
+ksp1=squeeze(ksp1);
+if length(size(ksp1)) == 3
+    [pha, swi_n, swi_p, mag] = phaserecon_v1(ksp1,ksp1,0.4,1,0.05);
+    % Necessary translations to match FDF images
+    swi_n=flipdim(flipdim(flipdim(swi_n,1),2),3);
+    swi_n=circshift(swi_n,[1,1,1]);
+    img=mag.*exp(1i*pha);
+elseif  length(size(ksp1)) == 4
+    for echo=1:size(ksp1,4)
+        [pha(:,:,:,echo), swi_n(:,:,:,echo), swi_p(:,:,:,echo), mag(:,:,:,echo)] = phaserecon_v1(ksp1(:,:,:,echo),ksp1(:,:,:,echo),0.4,1,0.05);
+        % Necessary translations to match FDF images
+        swi_n(:,:,:,echo)=flipdim(flipdim(flipdim(swi_n(:,:,:,echo),1),2),3);
+        swi_n(:,:,:,echo)=circshift(swi_n(:,:,:,echo),[1,1,1]);
+        img(:,:,:,echo)=mag(:,:,:,echo).*exp(1i*pha(:,:,:,echo));
+    end
+    
+    
+end
 if ~isempty(ksp2)
-    [pha2, swi_n2, swi_p2, mag2] = phaserecon_v1(ksp2,ksp2,0.4,1, ...
-                                                 0.05);
-    swi_n2=flipdim(flipdim(flipdim(swi_n2,1),2),3);
-    swi_n2=circshift(swi_n2,[1,1,1]);
-swi_n = (swi_n+swi_n2)/2;
-    swi_p = (swi_p+swi_p2)/2;
-img = (img+ (mag2.*exp(1i*pha2)))/2;
-
-		
+    if length(size(ksp1)) == 3
+        
+        [pha2, swi_n2, swi_p2, mag2] = phaserecon_v1(ksp2,ksp2,0.4,1, ...
+            0.05);
+        swi_n2=flipdim(flipdim(flipdim(swi_n2,1),2),3);
+        swi_n2=circshift(swi_n2,[1,1,1]);
+    elseif  length(size(ksp1)) == 4
+        for echo=1:size(ksp1,4)
+            [pha2(:,:,:,echo), swi_n2(:,:,:,echo), swi_p2(:,:,:,echo), mag2(:,:,:,echo)] = phaserecon_v1(ksp2(:,:,:,echo),ksp2(:,:,:,echo),0.4,1,0.05);
+            % Necessary translations to match FDF images
+            swi_n2(:,:,:,echo)=flipdim(flipdim(flipdim(swi_n2(:,:,:,echo),1),2),3);
+            swi_n2(:,:,:,echo)=circshift(swi_n2(:,:,:,echo),[1,1,1]);
+            img2(:,:,:,echo)=mag2(:,:,:,echo).*exp(1i*pha2(:,:,:,echo));
+        end
+        
+        % Combine input 1 and 2
+        swi_n = (swi_n+swi_n2)/2;
+        swi_p = (swi_p+swi_p2)/2;
+        img = (img+ (mag2.*exp(1i*pha2)))/2;
+        
+    end
 end
 
 if exist(out,'file')~=2 && ~isdir(out)
@@ -128,20 +151,20 @@ if exist(out,'file')
 end
 
 
-    save_nii(make_nii(swi_n,voxelsize,[],16),out)
+save_nii(make_nii(swi_n,voxelsize,[],16),out)
 
 
 if swipos
-   swi_p=flipdim(flipdim(flipdim(swi_p,1),2),3);
-   swi_p=circshift(swi_p,[1,1,1]);
-   outp = regexprep(out,'neg','pos');
-
-        save_nii(make_nii(swi_p,voxelsize,[],16),outp)
-
+    swi_p=flipdim(flipdim(flipdim(swi_p,1),2),3);
+    swi_p=circshift(swi_p,[1,1,1]);
+    outp = regexprep(out,'neg','pos');
+    
+    save_nii(make_nii(swi_p,voxelsize,[],16),outp)
+    
 end
 
 if saveRI
-        save_nii(make_nii(real(img),voxelsize,[],16),regexprep(out,'neg','real'))
-        save_nii(make_nii(imag(img),voxelsize,[],16),regexprep(out,'neg','imag'))
-
+    save_nii(make_nii(real(img),voxelsize,[],16),regexprep(out,'neg','real'))
+    save_nii(make_nii(imag(img),voxelsize,[],16),regexprep(out,'neg','imag'))
+    
 end
